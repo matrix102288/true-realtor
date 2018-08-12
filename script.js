83d80cde8e6eccc0e3d13689b60214528da10bcf
Joshaun <matrix102288@gmail.com>
2018-08-12 18:11:57 -0400

    add js

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const menu = document.getElementById('onMouse');
+
+const cursorEvent = () => menu.style.cursor = 'pointer';
+
+menu.addEventListener('mouseover', cursorEvent());
+
+
+const menu1 = document.getElementById('onMouse1');
+const cursorEvent1 = () => menu1.style.cursor = 'pointer';
+menu1.addEventListener('mouseover', cursorEvent1());
+
+const menu2 = document.getElementById('onMouse2');
+const cursorEvent2 = () => menu2.style.cursor = 'pointer';
+menu2.addEventListener('mouseover', cursorEvent2());
+
+const menu3 = document.getElementById('onMouse3');
+const cursorEvent3 = () => menu3.style.cursor = 'pointer';
+menu3.addEventListener('mouseover', cursorEvent3());
+
+const menu4 = document.getElementById('onMouse4');
+const cursorEvent4 = () => menu4.style.cursor = 'pointer';
+menu4.addEventListener('mouseover', cursorEvent4());
